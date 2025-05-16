6a98d503699af865979b027a6e39e01c665898f0
zaron4k <76218026+zaron4k@users.noreply.github.com>
2025-05-16 12:25:32 +0300

    Add files via upload

---
+++ ConsoleApp4/Handler_2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Handler_2
+    {
+        public void Message()
+        {
+            Console.WriteLine("Точно, уже 7!");
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassCounter counter = new ClassCounter();
+            Handler_1 handler_1 = new Handler_1();
+            Handler_2 handler_2 = new Handler_2();
+            counter.Count();
+            counter.onCount += handler_1.Message;
+            counter.onCount += handler_2.Message;
+            counter.onCount += counter.Message;
+            counter.Count();
+            Console.Read();
+        }
+    }
+}
